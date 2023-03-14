b1e7cda544cdf743f7d5496bbadb9cd621ed30a4
jon-cron <jonathanacm95@gmail.com>
2023-03-14 16:42:50 -0600

    firebase init

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// NOTE npm install firebase then do that following
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFdY1DuUKUcXakVGQId2dq4Y2FoHEmpAc",
+  authDomain: "thedojo-a522b.firebaseapp.com",
+  projectId: "thedojo-a522b",
+  storageBucket: "thedojo-a522b.appspot.com",
+  messagingSenderId: "352420739802",
+  appId: "1:352420739802:web:6f7738990eb7f75f0643e3",
+};
+
+firebase.initializeApp(firebaseConfig);
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export { projectAuth, projectFirestore };
