45dfb52f467bfef431a2ab7406995f0d40e83fed
jon-cron <jonathanacm95@gmail.com>
2023-03-15 10:33:46 -0600

    project error handling

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,5 +1,24 @@
 import React from "react";
 import "./Project.css";
+import { useDocument } from "../../hooks/useDocument.js";
+import { useParams } from "react-router-dom";
 export default function Project() {
-  return <div>Project</div>;
+  const { id } = useParams();
+  const { document, error } = useDocument("projects", id);
+  console.log(document, error);
+
+  // NOTE this is another way of conditionally rendering errors to the dom
+  // if(error){
+  //   return <div className="error">{error}</div>
+  // }
+  // if(!document){
+  //   return <div className="loading">loading...</div>
+  // }
+
+  return (
+    <div className="project-details">
+      {error && <div className="error">{error}</div>}
+      {document && <h1>{document.name}</h1>}
+    </div>
+  );
 }
