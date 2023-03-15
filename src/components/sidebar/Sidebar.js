4e9f2cc108bc5130d906f6f85dec470b2f77a870
jon-cron <jonathanacm95@gmail.com>
2023-03-15 07:11:49 -0600

    navigating based on auth

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,12 +3,14 @@ import "./Sidebar.css";
 import DashboardIcon from "../../assets/dashboard_icon.svg";
 import AddIcon from "../../assets/add_icon.svg";
 import { NavLink } from "react-router-dom";
+import { useAuthContext } from "../../hooks/useAuthContext.js";
 export default function Sidebar() {
+  const { user } = useAuthContext();
   return (
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
-          <p>Hey User</p>
+          <p>Hey {user?.displayName}</p>
         </div>
         <nav className="links">
           <ul>
