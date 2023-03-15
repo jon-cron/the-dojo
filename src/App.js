6ec0f8e09fd2e71b8a8ed5040c4f2a22f52c6259
jon-cron <jonathanacm95@gmail.com>
2023-03-15 04:35:22 -0600

    dashboard made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css'
-
+import "./App.css";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App
+export default App;
