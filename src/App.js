cda11777c06846d95c1268bda4fbf64c32a9264f
jon-cron <jonathanacm95@gmail.com>
2023-03-15 05:05:09 -0600

    navbar and sidebar comps

--- src/App.js
+++ src/App.js
@@ -11,18 +11,22 @@ import Login from "./pages/login/Login.js";
 import Signup from "./pages/signup/Signup.js";
 import Create from "./pages/create/Create.js";
 import Project from "./pages/project/Project.js";
+import Navbar from "./components/navbar/Navbar.js";
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route exact path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/projects/:id" element={<Project />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <div className="container">
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/projects/:id" element={<Project />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
       </Router>
     </div>
   );
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Sidebar.css";
+import DashboardIcon from "../../assets/dashboard_icon.svg";
+import AddIcon from "../../assets/add_icon.svg";
+import { NavLink } from "react-router-dom";
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <div className="user">
+          <p>Hey User</p>
+        </div>
+        <nav className="links">
+          <ul>
+            <li>
+              <NavLink to="/">
+                <img src={DashboardIcon} alt="dashboard" />
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/create">
+                <img src={AddIcon} alt="create" />
+                <span>Create</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
