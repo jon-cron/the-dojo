637810a515b27014d5a6f819457474cbcf182146
jon-cron <jonathanacm95@gmail.com>
2023-03-15 06:29:18 -0600

    changing online status

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./Navbar.css";
 import Temple from "../../assets/temple.svg";
 import { Link } from "react-router-dom";
+import { useLogout } from "../../hooks/useLogout.js";
 export default function Navbar() {
+  const { logout, isPending } = useLogout();
   return (
     <div className="navbar">
       <ul>
@@ -17,7 +19,16 @@ export default function Navbar() {
           <Link to="/signup">Signup</Link>
         </li>
         <li>
-          <button className="btn">Logout</button>
+          {!isPending && (
+            <button className="btn" onClick={logout}>
+              Logout
+            </button>
+          )}
+          {isPending && (
+            <button disabled className="btn">
+              logging out
+            </button>
+          )}
         </li>
       </ul>
     </div>
