9b95a0768b6f01a6c4078839f5037db4ef84377d
jon-cron <jonathanacm95@gmail.com>
2023-03-15 05:21:34 -0600

    signup form made

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,49 @@
-import React from "react";
+import { useState } from "react";
 import "./Signup.css";
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+  return (
+    <form onSubmit={handleSubmit} className="auth-form">
+      <h2>Sign up</h2>
+      <label>
+        <span>Email</span>
+        <input
+          required
+          value={email}
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Password</span>
+        <input
+          required
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Display Name</span>
+        <input
+          required
+          value={displayName}
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Profile Image</span>
+        <input required type="file" />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  );
 }
