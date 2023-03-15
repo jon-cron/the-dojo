01dc2711993c3e376cc3cadabe5cb51d6bc45e7f
jon-cron <jonathanacm95@gmail.com>
2023-03-15 06:01:02 -0600

    img upload notes

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -24,7 +24,9 @@ export const useSignup = () => {
       }
       // NOTE before this line of code we signup and get authenticated, we can use that auth to upload a file to storage. After we upload that picture to storage we can take that imgURL add it to our profile image.
       const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`;
+      // NOTE we get the img back from firebase
       const img = await projectStorage.ref(uploadPath).put(thumbnail);
+      // NOTE then we drill into the img and get the imgUrl to use on our profile
       const imgUrl = await img.ref.getDownloadURL();
 
       // add display name to user
